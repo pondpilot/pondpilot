8e5019887c2416fe08b0bd15c36be22683ea1921
Andrii Butko <63796184+booandrew@users.noreply.github.com>
2025-03-06 14:17:21 +0100

    Update Vite configuration to fix PWA issue (#30)

--- vite.config.mjs
+++ vite.config.mjs
@@ -80,6 +80,24 @@ export default defineConfig(({ mode }) => {
           display: 'standalone',
           background_color: '#ffffff',
           theme_color: '#000000',
+          icons: [
+            {
+              src: '192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: '512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
         },
       }),
       svgr(),
