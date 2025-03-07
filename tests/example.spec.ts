d81ccfc9c0a4e80d26668d07604ac55f6bb12e25
Andrii Butko <booandrew23@gmail.com>
2025-03-07 13:56:24 +0100

    Add Playwright testing setup and update configurations

---
+++ tests/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('my test suite', () => {
+  test('text about ', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await page.waitForTimeout(1000);
+
+    await page.click('id=add-query');
+
+    await page.fill('.cm-content', 'select 1');
+    await page.waitForTimeout(1000);
+
+    await page.click('data-testid=run-query-button');
+    await page.waitForTimeout(1000);
+
+    await expect(page.getByText('Query ran successfully')).toBeVisible();
+  });
+});
