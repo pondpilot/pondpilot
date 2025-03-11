0b3d975bd9ff49feff744b849680405acdb889e9
mishamsk <mishamsk@gmail.com>
2025-03-10 21:03:14 -0400

    move reporting to playwright config file fix workflow_run dependency specs

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: './playwright-report' }],
+    ['junit', { outputFile: './playwright-report/results.xml' }],
+  ],
   /* Configure projects for major browsers */
   projects: [
     {
