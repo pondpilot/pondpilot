dab5267ed7797d46dda71234a02c25d2f3557285
mishamsk <mishamsk@gmail.com>
2025-03-26 13:03:26 -0400

    fix formatter error

--- src/features/data-viewer/components/pagination-control.tsx
+++ src/features/data-viewer/components/pagination-control.tsx
@@ -24,25 +24,25 @@ export const PaginationControl = memo(
           : `${formatNumber(rowCount)} rows`
         : '0 rows';
     return (
-    <Group
-      bg="background-primary"
-      className="h-11 rounded-full min-w-40 px-4 py-2 shadow-xl shadow-transparentBrandBlue-008 dark:shadow-transparentBrandBlue-008 border border-borderLight-light dark:border-borderLight-dark"
-      c="text-secondary"
-      justify="space-between"
-    >
-      <Group className="text-sm" data-testid={setDataTestId('pagination-control-out-of')}>
-        {outOf}
+      <Group
+        bg="background-primary"
+        className="h-11 rounded-full min-w-40 px-4 py-2 shadow-xl shadow-transparentBrandBlue-008 dark:shadow-transparentBrandBlue-008 border border-borderLight-light dark:border-borderLight-dark"
+        c="text-secondary"
+        justify="space-between"
+      >
+        <Group className="text-sm" data-testid={setDataTestId('pagination-control-out-of')}>
+          {outOf}
+        </Group>
+        <Group gap={0}>
+          <ActionIcon onClick={onPrevPage}>
+            <IconChevronLeft />
+          </ActionIcon>
+          <ActionIcon onClick={onNextPage}>
+            <IconChevronRight />
+          </ActionIcon>
+        </Group>
       </Group>
-      <Group gap={0}>
-        <ActionIcon onClick={onPrevPage}>
-          <IconChevronLeft />
-        </ActionIcon>
-        <ActionIcon onClick={onNextPage}>
-          <IconChevronRight />
-        </ActionIcon>
-      </Group>
-    </Group>
-  );
+    );
   },
   (prevProps, nextProps) =>
     prevProps.currentPage === nextProps.currentPage &&
