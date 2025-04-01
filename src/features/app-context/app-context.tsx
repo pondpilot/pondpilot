3e60d4235c5c373fc74a923257bd045275f44daf
Andrii Butko <booandrew23@gmail.com>
2025-04-01 14:14:22 +0200

    WIP: data view/idb store management

--- src/features/app-context/app-context.tsx
+++ src/features/app-context/app-context.tsx
@@ -223,7 +223,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
 export const useAppContext = () => {
   const context = useContext(AppContext);
   if (!context) {
-    throw new Error('useSessionContext must be used within a SessionProvider');
+    throw new Error('useAppContext must be used within a AppContextProvider');
   }
   return context;
 };
