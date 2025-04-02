58c109ac1f4176c79a49a3a95db7a4a671463947
Andrii Butko <booandrew23@gmail.com>
2025-04-02 19:00:43 +0200

    [Infra bug]: fix eslint imports setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,16 @@ module.exports = {
   },
   plugins: ['unused-imports'],
   rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
     'react/react-in-jsx-scope': 'off',
     'import/extensions': 'off',
     'consistent-return': 'off',
