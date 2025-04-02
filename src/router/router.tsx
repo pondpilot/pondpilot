1f178de34987c8b6bd532b54b13c95b1af0df562
mishamsk <mishamsk@gmail.com>
2025-04-01 20:23:54 -0400

    🔥 remove deprecated and unused error fallback

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,7 @@
 import { Layout } from '@components/layout';
-import { AppErrorFallback, DataViewErrorFallback } from '@components/error-fallback';
+import { AppErrorFallback } from '@components/error-fallback';
 import { MainPage } from '@pages/main-page';
 import { SettingsPage } from '@pages/settings-page';
-import { ErrorBoundary } from 'react-error-boundary';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
@@ -13,11 +12,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: (
-          <ErrorBoundary FallbackComponent={DataViewErrorFallback}>
-            <MainPage />
-          </ErrorBoundary>
-        ),
+        element: <MainPage />,
       },
       {
         path: 'settings',
