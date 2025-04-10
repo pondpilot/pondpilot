a4e661a9d6ad946c5ab1dfbb68a69c549d40b364
Andrii Butko <booandrew23@gmail.com>
2025-04-10 21:49:26 +0200

    [WIP] Data view rework

--- src/features/tab-view/file-data-source-tab-view.tsx
+++ src/features/tab-view/file-data-source-tab-view.tsx
@@ -1,9 +1,12 @@
-import { memo } from 'react';
+import { memo, useEffect, useMemo, useState } from 'react';
 import { Allotment } from 'allotment';
 import { FileDataSourceTab } from '@models/tab';
-import { updateTabDataViewLayout } from '@store/init-store';
+import { updateTabDataViewLayout, useInitStore } from '@store/init-store';
+import { getFlatFileDataAdapterApi } from '@controllers/db/data-view';
+import { useInitializedDuckDBConnection } from '@features/duckdb-context/duckdb-context';
+import { AsyncRecordBatchStreamReader, RecordBatch } from 'apache-arrow';
+import { getArrowTableSchema } from '@utils/arrow/schema';
 import { DataView } from './components/data-view';
-import { useDataView } from './hooks/useDataView';
 
 interface FileDataSourceTabViewProps {
   tab: FileDataSourceTab;
@@ -11,23 +14,88 @@ interface FileDataSourceTabViewProps {
 }
 
 export const FileDataSourceTabView = memo(({ tab, active }: FileDataSourceTabViewProps) => {
-  const setPanelSize = ([_, table]: number[]) => {
-    updateTabDataViewLayout(tab, {
-      ...tab.dataViewLayout,
-      dataViewPaneHeight: table,
-    });
+  const { conn } = useInitializedDuckDBConnection();
+  const cache = useInitStore((state) => state.dataViewCache);
+  const dataSource = useInitStore((state) => state.dataSources.get(tab.dataSourceId));
+  const sourceFile = useInitStore((state) =>
+    dataSource?.fileSourceId ? state.localEntries.get(dataSource?.fileSourceId) : null,
+  );
+  const dataViewAdapter = useMemo(() => {
+    if (dataSource && dataSource.type !== 'attached-db' && sourceFile) {
+      return getFlatFileDataAdapterApi(dataSource, tab.id, sourceFile);
+    }
+    return null;
+  }, [dataSource, sourceFile, tab.id]);
+
+  const cachedData = dataViewAdapter?.getCacheKey && cache.get(dataViewAdapter.getCacheKey());
+
+  const [reader, setReader] = useState<AsyncRecordBatchStreamReader | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<any[]>(cachedData?.data || []);
+  const [columns, setColumns] = useState<any[]>(cachedData?.columns || []);
+
+  const fetchData = async () => {
+    if (!reader) {
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const batch = await reader.next();
+      const batchValue: RecordBatch = batch.value;
+
+      if (batchValue) {
+        const hasColumns = columns.length;
+
+        const tableData = batchValue.toArray().map((row) => row.toJSON());
+        const tableColumns = (hasColumns ? columns : getArrowTableSchema(batchValue)) || [];
+
+        setData((prevData) => [...prevData, ...tableData]);
+        setColumns(tableColumns);
+      } else {
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
   };
-  const { fetchedData, isQueryRunning } = useDataView(tab);
+
+  useEffect(() => {
+    if (!dataViewAdapter || !conn || reader) {
+      return;
+    }
+    const createReader = async () => {
+      // here we can show the loader and not render the table
+      const _reader = await dataViewAdapter.getReader(conn, []);
+      setReader(_reader);
+    };
+    createReader();
+  }, [dataViewAdapter, conn]);
 
   return (
     <div className="h-full relative">
       <Allotment
         vertical
-        onDragEnd={setPanelSize}
+        onDragEnd={([_, table]: number[]) => {
+          updateTabDataViewLayout(tab, {
+            ...tab.dataViewLayout,
+            dataViewPaneHeight: table,
+          });
+        }}
         defaultSizes={[0, tab.dataViewLayout.dataViewPaneHeight]}
       >
         <Allotment.Pane preferredSize={tab.dataViewLayout.dataViewPaneHeight} minSize={120}>
-          <DataView data={fetchedData} isLoading={isQueryRunning} active={active} />
+          {dataViewAdapter && (
+            <DataView
+              data={data}
+              columns={columns}
+              isActive={active}
+              isScriptTab={false}
+              isLoading={isLoading}
+            />
+          )}
         </Allotment.Pane>
       </Allotment>
     </div>
