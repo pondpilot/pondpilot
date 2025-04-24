1e0c729ef0b03e690d7c35e2071d077909c49dc7
Andrii Butko <63796184+booandrew@users.noreply.github.com>
2025-04-24 23:46:36 +0200

    update api url
    Co-authored-by: Mike Perlov <mishamsk@gmail.com>

--- src/app-urls.ts
+++ src/app-urls.ts
@@ -1,5 +1,5 @@
 export const APP_GITHUB_URL = 'https://github.com/pondpilot/pondpilot';
-export const API_GITHUB_URL = 'https://api.github.com/repos/pondpilot/pondpilot/releases/tags';
+export const APP_RELEASE_TAGS_GITHUB_API_URL = 'https://api.github.com/repos/pondpilot/pondpilot/releases/tags';
 export const APP_OPEN_ISSUES_URL = `${APP_GITHUB_URL}/issues`;
 export const APP_SUPPORT_URL = APP_OPEN_ISSUES_URL;
 export const APP_DOCS_URL = `${APP_GITHUB_URL}/blob/main/README.md`;
