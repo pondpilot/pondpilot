3e5bf510a5f52eb289f39fda7424041f26942c00
Andrii Butko <booandrew23@gmail.com>
2025-04-24 17:03:47 +0200

    remove release notes modal

--- src/components/whats-new-modal/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './whats-new-modal';
--- src/hooks/use-init-modals.tsx
+++ src/hooks/use-init-modals.tsx
@@ -12,34 +12,16 @@ export const useInitModals = () => {
     defaultValue: false,
   });
 
-  const [whatsNewVersionShown, setWhatsNewVersionShown] = useLocalStorage({
-    key: LOCAL_STORAGE_KEYS.WHATS_NEW_VERSION_SHOWN,
-  });
-
   useDidUpdate(() => {
     if (appLoadState === 'ready') {
-      // If a user is using the app for the first time, show the Onboarding modal, not the Release Notes
+      // If a user is using the app for the first time, show the Onboarding modal
       if (!isOnboardingShown) {
-        setWhatsNewVersionShown(__VERSION__);
-
         const modalId = modals.open({
           ...ONBOARDING_MODAL_OPTIONS,
           onClose: () => setIsOnboardingShown(true),
           children: <OnboardingModal onClose={() => modals.close(modalId)} />,
         });
-        return;
       }
-
-      // if (whatsNewVersionShown && isVersionGreater(__VERSION__, whatsNewVersionShown)) {
-      //   const modalId = modals.open({
-      //     ...WHATS_NEW_MODAL_OPTIONS,
-      //     onClose: () => setWhatsNewVersionShown(__VERSION__),
-      //     children: <WhatsNewModal onClose={() => modals.close(modalId)} />,
-      //   });
-      //   return;
-      // }
-      // Set the current version by default
-      setWhatsNewVersionShown(__VERSION__);
     }
   }, [appLoadState]);
 };
