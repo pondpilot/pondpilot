7b160e0fc7872ebda934dfd6cd2625b9d2caf927
Andrii Butko <booandrew23@gmail.com>
2025-04-25 15:14:38 +0200

    remove fixtures

--- tests/integration/onboarding/onboarding.spec.ts
+++ tests/integration/onboarding/onboarding.spec.ts
@@ -1,26 +1,27 @@
-import { expect } from '@playwright/test';
-import { test } from '../fixtures/onboarding';
+import test, { expect } from '@playwright/test';
 
-test('Onboarding modal is displayed on page load', async ({
-  page,
-  closeOnboardingModal,
-  openOnboardingModal,
-  onboardingModal,
-}) => {
+test('Onboarding modal is displayed on page load', async ({ page }) => {
   // Navigate to the application
   await page.goto('/');
+
+  const onboardingModal = page.getByTestId('onboarding-modal');
+  const submitButton = page.getByTestId('onboarding-modal-submit-button');
+  const showOnboardingButton = page.getByTestId('start-guide-action-onboarding');
 
   // Check if the onboarding modal is visible on initial load
   await expect(onboardingModal).toBeVisible();
 
   // Close the modal and verify it's gone
-  await closeOnboardingModal();
+  await submitButton.click();
+  await expect(onboardingModal).toBeHidden();
 
   // Check if the onboarding modal is not visible after reloading
   await page.reload();
   await expect(onboardingModal).toBeHidden();
 
   // Check if the onboarding modal is visible on clicking the button
-  await openOnboardingModal();
+  await expect(showOnboardingButton).toBeVisible();
+  await showOnboardingButton.click();
+
   await expect(onboardingModal).toBeVisible();
 });
