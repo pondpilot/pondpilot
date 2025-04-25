23a924bd9635c16079163ded273c927c76f6c419
Andrii Butko <booandrew23@gmail.com>
2025-04-25 14:33:10 +0200

    fix tests

--- tests/integration/fixtures/page.ts
+++ tests/integration/fixtures/page.ts
@@ -1,9 +1,10 @@
+import { LOCAL_STORAGE_KEYS } from '@consts/local-storage';
 import { test as base, expect, Page } from '@playwright/test';
 
 const waitForAppReady = async (page: Page) => {
   // Wait for the app to be ready
   const appStatus = page.getByTestId('app-state');
-  await expect(appStatus).toHaveAttribute('data-app-load-state', 'ready', { timeout: 15_000 });
+  await expect(appStatus).toHaveAttribute('data-app-load-state', 'ready');
 };
 
 type PageFixtures = {
@@ -14,6 +15,14 @@ export const test = base.extend<PageFixtures>({
   page: async ({ page }, use) => {
     // ---------- BEFORE EACH TEST ----------
     await page.goto('/');
+
+    await page.evaluate(
+      (key) => localStorage.setItem(key, 'true'),
+      LOCAL_STORAGE_KEYS.ONBOARDING_SHOWN,
+    );
+
+    await page.reload();
+
     await waitForAppReady(page);
 
     await use(page);
