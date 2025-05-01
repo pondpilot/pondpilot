8f128038264bdbb1c1ccd907cac0195051f4fd97
mishamsk <mishamsk@gmail.com>
2025-05-01 18:56:59 -0400

    🐛 make sure Opt-Q on loading overlay works

--- src/components/data-loading-overlay/data-loading-overlay.tsx
+++ src/components/data-loading-overlay/data-loading-overlay.tsx
@@ -1,5 +1,8 @@
+import { HotkeyPill } from '@components/hotkey-pill';
 import { LoadingOverlay } from '@components/loading-overlay';
-import { Stack, Loader, Button, Text } from '@mantine/core';
+import { useOsModifierIcon } from '@hooks/use-os-modifier-icon';
+import { Stack, Loader, Button, Text, Group } from '@mantine/core';
+import { useHotkeys } from '@mantine/hooks';
 
 interface DataLoadingOverlayProps {
   title: string;
@@ -7,24 +10,42 @@ interface DataLoadingOverlayProps {
   visible: boolean;
 }
 
-export const DataLoadingOverlay = ({ onCancel, visible, title }: DataLoadingOverlayProps) => (
-  <LoadingOverlay visible={visible}>
-    <Stack align="center" gap={4} bg="background-primary" className="p-8 pt-4 rounded-2xl">
-      <Loader size={24} color="text-secondary" />
-      <Text c="text-primary" className="text-2xl font-medium">
-        {title}
-      </Text>
-      <span className="text-textSecondary-light dark:text-textSecondary-dark font-medium">
-        Press{' '}
-        <Button
-          c="text-primary"
-          onClick={onCancel}
-          className="bg-backgroundSecondary-light dark:bg-backgroundSecondary-dark hover:bg-backgroundTertiary-light dark:hover:bg-backgroundTertiary-dark"
-        >
-          Cancel
-        </Button>{' '}
-        or ⌥ Q to abort processing
-      </span>
-    </Stack>
-  </LoadingOverlay>
-);
+export const DataLoadingOverlay = ({ onCancel, visible, title }: DataLoadingOverlayProps) => {
+  const { option } = useOsModifierIcon();
+
+  useHotkeys([
+    [
+      'Alt+Q',
+      () => {
+        if (visible) {
+          onCancel();
+        }
+      },
+    ],
+  ]);
+
+  return (
+    <LoadingOverlay visible={visible}>
+      <Stack align="center" gap={4} bg="background-primary" className="p-8 pt-4 rounded-2xl">
+        <Loader size={24} color="text-secondary" />
+        <Text c="text-primary" className="text-2xl font-medium">
+          {title}
+        </Text>
+        <span className="text-textSecondary-light dark:text-textSecondary-dark font-medium">
+          Press{' '}
+          <Group align="center" className="inline-flex">
+            <Button
+              c="text-primary"
+              onClick={onCancel}
+              className="bg-backgroundSecondary-light dark:bg-backgroundSecondary-dark hover:bg-backgroundTertiary-light dark:hover:bg-backgroundTertiary-dark"
+            >
+              Cancel
+            </Button>{' '}
+            or <HotkeyPill variant="secondary" value={[option, 'Q']} />
+          </Group>{' '}
+          to abort processing
+        </span>
+      </Stack>
+    </LoadingOverlay>
+  );
+};
