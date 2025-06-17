168c72125868c5c74bc7d0f1b1e29213a4657d2e
Alex Mazanov <alexandr.mazanov@gmail.com>
2025-06-17 09:27:04 -0400

    Remove unused component
    Signed-off-by: Alex Mazanov <alexandr.mazanov@gmail.com>

--- src/features/data-explorer/components/index.ts
+++ src/features/data-explorer/components/index.ts
@@ -1,7 +1,6 @@
 export * from './connection-state-icon';
 export * from './data-explorer-filters';
 export * from './database-label';
-export * from './database-section';
 export * from './file-system-tree-builder';
 export * from './data-explorer-section';
 export * from './data-explorer-content';
