3667a6d6404e11ea207d7bee1c9c89ab0f94b894
Alex Mazanov <alexandr.mazanov@gmail.com>
2025-06-17 18:52:00 -0400

    Remove dead code
    Signed-off-by: Alex Mazanov <alexandr.mazanov@gmail.com>

--- src/features/data-explorer/components/index.ts
+++ src/features/data-explorer/components/index.ts
@@ -1,6 +1,4 @@
-export * from './connection-state-icon';
 export * from './data-explorer-filters';
-export * from './database-label';
 export * from './file-system-tree-builder';
 export * from './data-explorer-section';
 export * from './data-explorer-content';
