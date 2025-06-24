63f3227fd3ad8efa1b66fc074f1cbbd11d1cf7fb
Andrii Butko <booandrew23@gmail.com>
2025-06-24 16:19:44 +0200

    Remove placeholder error throw in TabView component

--- src/features/tab-view/tab-view.tsx
+++ src/features/tab-view/tab-view.tsx
@@ -15,8 +15,6 @@ export const TabView = () => {
   const tabToTypeMap = useTabTypeMap();
   const activeTabId = useAppStore.use.activeTabId();
 
-  throw new Error('TabView is not implemented');
-
   // Use tab cache t avoid rendering all tabs at once
   const { addToCache, isTabCached } = useTabCache(TAB_CACHE_SIZE);
 
